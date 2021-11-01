8166fce422753b2bfedfbca4e275681fbb31dcda
peng <song.peng@bizerba.com>
2021-11-01 10:15:12 +0800

    flask: redirect to external domain

--- Python301_urlRequest/03_flask.py
+++ Python301_urlRequest/03_flask.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask,redirect
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    return 'Hello, World!'
+
+@app.route('/google')
+def google():
+    return redirect("http://www.google.com")
+
+@app.route('/eastmoney')
+def eastmoney():
+    return redirect("http://stock.jrj.com.cn/share,000830,gdhs.shtml")
 
 app.run(host='0.0.0.0', port=8080)
