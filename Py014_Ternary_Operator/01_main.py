1b3cf779fecacab5ad729a012c956201550e3732
songpeng22 <songpeng24@msn.com>
2024-08-15 10:55:18 +0800

    ternary operator/三元运算符

---
+++ Py014_Ternary_Operator/01_main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/08/15
+# @Author  : peng song
+# @Email   : songpeng24@msn.com
+# @File    : Py014_Ternary_Operator\01_main.py
+# @Desc    : ternary operator/三元表达式 -> x if x > y else y
+
+# Program to demonstrate ternary operator
+a = 10
+b = 20
+
+# python ternary operator
+min = "a is minimum" if a < b else "b is minimum"
+
+print(min)
